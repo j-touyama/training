3cf5a263a3a8e3a888d9405b54289d3595e62faa
j-touyama <87508483+j-touyama@users.noreply.github.com>
2022-03-01 18:03:55 +0900

    Update EditController.java

--- src/main/java/com/example/demo/employee/controller/EditController.java
+++ src/main/java/com/example/demo/employee/controller/EditController.java
@@ -24,6 +24,14 @@ public class EditController {
 	private static final String UPDATE = "更新"; 
 	private static final String DELETE = "削除"; 
 
+	/**
+	 * 新規登録の処理
+	 * @param form
+	 * @param result
+	 * @param model
+	 * @return
+	 * @throws Exception
+	 */
 	@RequestMapping(value="/insert", params="regist", method=RequestMethod.POST)
 	public String insertRegist(@ModelAttribute("form") @Validated(GroupOrder.class) UserForm form,
 			BindingResult result, Model model) throws Exception {
@@ -40,6 +48,13 @@ public class EditController {
 		}
 	}
 
+	/**
+	 * 更新の処理
+	 * @param form
+	 * @param result
+	 * @param model
+	 * @return
+	 */
 	@RequestMapping(value="/update", params="regist", method=RequestMethod.POST)
 	public String updateRegist(@ModelAttribute("form") @Validated(GroupOrder.class) UserForm form,
 			BindingResult result, Model model) {
@@ -54,14 +69,20 @@ public class EditController {
 			e.printStackTrace();
 			return "/error/index";
 		}
-
 	}
 
+	/**
+	 * 削除の処理
+	 * @param form
+	 * @param result
+	 * @param model
+	 * @return
+	 */
 	@RequestMapping(value="/delete", params="regist", method=RequestMethod.POST)
 	public String deleteRegist(@ModelAttribute("form") @Validated(GroupOrder.class) UserForm form,
 			BindingResult result, Model model) {
 		try {
-			service.deleteUser(form.getStaff_code());
+			service.deleteUser(form.getStaffCode());
 			model.addAttribute("editType", DELETE);
 			return "/admin/result";
 		} catch (Exception e) {
@@ -70,8 +91,21 @@ public class EditController {
 		}
 	}
 
+	/**
+	 * 戻るボタン押下時の処理
+	 * @return
+	 */
 	@RequestMapping(value={"/insert", "/update", "/delete"}, params="back", method=RequestMethod.POST)
 	public String back() {
 		return "redirect:/employee/admin/index";
 	}
+
+	/**
+	 * URLを直叩きした時の処理
+	 */
+	@RequestMapping(value="/*", method=RequestMethod.GET)
+	public String getMapping() {
+		return "redirect:/employee";
+	}
+
 }
